f30740fdf0e5fc9dd5b2bcd953d463649ac1adfe
DaS-WUnder <david.sander@web.de>
2014-09-27 22:33:51 +0200

    Changed some colors and changes/added some comments

--- themes/purpleTheme.py
+++ themes/purpleTheme.py
@@ -3,17 +3,17 @@
 class Color(DefaultColor):
     USERNAME_FG = 15 # white 
     USERNAME_BG = 54 #purple
-    USERNAME_ROOT_BG = 124 #red
+    USERNAME_ROOT_BG = 160 #red
 
-    HOSTNAME_FG = 232 
-    HOSTNAME_BG = 250 
+    HOSTNAME_FG = 232  #grey 
+    HOSTNAME_BG = 250  #lighter grey
 
     HOME_SPECIAL_DISPLAY = True
-    HOME_BG = 91# blueish
-    HOME_FG = 15  # white
-    PATH_BG = 237  # dark grey
-    PATH_FG = 250  # light grey
-    CWD_FG = 254  # nearly-white grey
+    HOME_BG = 91 #some purple
+    HOME_FG = 15  #white
+    PATH_BG = 237  #dark grey
+    PATH_FG = 250  #light grey
+    CWD_FG = 254  #nearly-white grey
     SEPARATOR_FG = 244
 
     READONLY_BG = 124
@@ -23,16 +23,17 @@ class Color(DefaultColor):
     SSH_FG = 254
 
     REPO_CLEAN_BG = 148  # a light green color
-    REPO_CLEAN_FG = 0  # black
+    REPO_CLEAN_FG = 0  #black
     REPO_DIRTY_BG = 160  #red
     REPO_DIRTY_FG = 15  #white
 
-    JOBS_FG = 39
-    JOBS_BG = 238
+    # same colors like in hostname
+    JOBS_FG = 232 #grey
+    JOBS_BG = 250 #lighter grey  
 
     CMD_PASSED_BG = 236
     CMD_PASSED_FG = 15
-    CMD_FAILED_BG = 161
+    CMD_FAILED_BG = 208 #some orange-yellow
     CMD_FAILED_FG = 15
 
     SVN_CHANGES_BG = 148
